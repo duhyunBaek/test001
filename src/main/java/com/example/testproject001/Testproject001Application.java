aff5110ab1c8da7a04b26a70256b37f9bc6b96d3
duhyunbaek <dh100@colosseum.kr>
2024-02-23 09:06:35 +0900

    firstCommit

---
+++ src/main/java/com/example/testproject001/Testproject001Application.java
@@ -0,0 +1,13 @@
+package com.example.testproject001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testproject001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testproject001Application.class, args);
+    }
+
+}
